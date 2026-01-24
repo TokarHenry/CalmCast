ffd3d08d695f49a33fa24199281fdd24194496d4
David Wilson <davidraywilson@live.com>
2026-01-24 13:12:28 -0600

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.calmapps.calmcast"
         minSdk = 28
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
